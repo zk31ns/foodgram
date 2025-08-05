4502ba1398d74241feab9f58d956ba37c1fc587f
Alexander Davydov <zk31ns@mail.ru>
2025-08-05 17:16:34 +0300

    исправлен permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,14 +8,16 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user and request.user.is_authenticated
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user and request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
 
 
 class IsSelfOrReadOnly(permissions.BasePermission):
@@ -24,6 +26,7 @@ class IsSelfOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj == request.user
+        )
