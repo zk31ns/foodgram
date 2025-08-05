30154a26a0e0e74a1a6d28f2c6fea599a32fd4fb
Alexander Davydov <zk31ns@mail.ru>
2025-08-05 14:37:18 +0300

    исправили admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db import models
 
 from users.models import Subscription
 
@@ -8,7 +9,7 @@ User = get_user_model()
 
 
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
         'username',
         'email',
@@ -16,9 +17,12 @@ class CustomUserAdmin(UserAdmin):
         'last_name',
         'is_staff',
         'is_superuser',
+        'subscribers_count',
+        'recipes_count',
     )
     search_fields = ('email', 'username', 'first_name', 'last_name')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
+
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (
@@ -40,6 +44,20 @@ class CustomUserAdmin(UserAdmin):
             ),
         }),
     )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            _subscribers_count=models.Count('following', distinct=True),
+            _recipes_count=models.Count('recipes', distinct=True)
+        )
+
+    @admin.display(description='Подписчики')
+    def subscribers_count(self, obj):
+        return obj._subscribers_count or 0
+
+    @admin.display(description='Рецепты')
+    def recipes_count(self, obj):
+        return obj._recipes_count or 0
 
 
 @admin.register(Subscription)
