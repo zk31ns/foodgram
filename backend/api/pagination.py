de873859ed2505d1b509249e60be3917a286b7ed
Alexander Davydov <zk31ns@mail.ru>
2025-08-05 17:12:12 +0300

    исправили pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,7 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
+from api.constants import DEFAULT_PAGE_SIZE
 
 
 class CustomPaginator(PageNumberPagination):
     """Пагинация с параметром limit и размером страницы по умолчанию 6."""
     page_size_query_param = 'limit'
-    page_size = 6
+    page_size = DEFAULT_PAGE_SIZE
