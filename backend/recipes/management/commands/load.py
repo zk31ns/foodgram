2ee2ac229e0c5058e11851eb205947ee68b389a3
Alexander Davydov <zk31ns@mail.ru>
2025-08-07 15:46:19 +0300

    изменили load.py

--- backend/recipes/management/commands/load.py
+++ backend/recipes/management/commands/load.py
@@ -64,9 +64,9 @@ class Command(BaseCommand):
 
         # Создание тегов
         tags_data = [
-            {"name": "Завтрак", "slug": "breakfast", "color": "#E26C2D"},
-            {"name": "Обед", "slug": "lunch", "color": "#49B64E"},
-            {"name": "Ужин", "slug": "dinner", "color": "#8775D2"},
+            {"name": "Завтрак", "slug": "breakfast"},
+            {"name": "Обед", "slug": "lunch"},
+            {"name": "Ужин", "slug": "dinner"},
         ]
 
         created_tags = 0
