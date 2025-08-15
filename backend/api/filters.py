eb554a1ddcadef3da651d8de365556f76ead5f8d
Alexander Davydov <zk31ns@mail.ru>
2025-08-16 02:42:11 +0300

    тестим

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,11 +5,11 @@ from recipes.models import Ingredient, Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
-    """Фильтр рецептов по тегам, автору, избранному и списку покупок."""
+    """
+    Фильтр рецептов по тегам, автору, избранному и списку покупок.
+    """
     tags = django_filters.CharFilter(method='filter_tags')
-    is_favorited = django_filters.BooleanFilter(
-        method='filter_is_favorited'
-    )
+    is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = django_filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
     )
@@ -17,45 +17,20 @@ class RecipeFilter(django_filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = [
-            'tags',
-            'author',
-            'is_favorited',
-            'is_in_shopping_cart'
-        ]
-
-    def filter_queryset(self, queryset):
-        """Явно применяем фильтры в нужном порядке."""
-        queryset = super().filter_queryset(queryset)
-
-        is_fav = self.data.get('is_favorited') == '1'
-        if is_fav and self.request.user.is_authenticated:
-            queryset = queryset.filter(
-                favorite_related__user=self.request.user
-            ).distinct()
-
-        is_in_cart = self.data.get('is_in_shopping_cart') == '1'
-        if is_in_cart and self.request.user.is_authenticated:
-            queryset = queryset.filter(
-                shoppingcart_related__user=self.request.user
-            ).distinct()
-
-        return queryset
+        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
 
     def filter_tags(self, queryset, name, value):
-        """Фильтрует рецепты по списку тегов."""
         tag_slugs = self.request.query_params.getlist('tags')
         if tag_slugs:
-            q_objects = Q()
-            for slug in tag_slugs:
-                q_objects |= Q(tags__slug=slug)
-            return queryset.filter(q_objects).distinct()
+            return queryset.filter(tags__slug__in=tag_slugs).distinct()
         return queryset
 
     def filter_is_favorited(self, queryset, name, value):
         """Фильтрует рецепты, добавленные в избранное."""
+        if isinstance(value, str):
+            value = value == '1'
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorite_related__user=self.request.user)
+            return queryset.filter(favorite_related__user=self.request.user).distinct()
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
@@ -64,6 +39,16 @@ class RecipeFilter(django_filters.FilterSet):
             return queryset.filter(
                 shoppingcart_related__user=self.request.user
             )
+        return queryset
+
+    def filter_queryset(self, queryset):
+        """Явно применяем фильтры в нужном порядке."""
+        queryset = super().filter_queryset(queryset)
+        if 'is_favorited' in self.data and self.data['is_favorited'] == '1' and self.request.user.is_authenticated:
+            queryset = queryset.filter(favorite_related__user=self.request.user).distinct()
+        if 'is_in_shopping_cart' in self.data and self.data[
+            'is_in_shopping_cart'] == '1' and self.request.user.is_authenticated:
+            queryset = queryset.filter(shoppingcart_related__user=self.request.user).distinct()
         return queryset
 
 
